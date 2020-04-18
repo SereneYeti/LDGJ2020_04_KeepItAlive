7871d38e377c85b32988da4b34eead2a6964262d
SereneYeti <40300212+SereneYeti@users.noreply.github.com>
2020-04-18 14:28:51 +0200

    Updated movement with  NavMesh
    Basic Point and Click Movement now working.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    NavMeshAgent agent;
+    //public float moveSpeed = 5f;
+    //public Rigidbody rb;
+    //Vector3 movement = new Vector3();
 
-    public float moveSpeed = 5f;
-
-    public Rigidbody rb;
-
-
-    Vector3 movement = new Vector3();
     private void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     public void Movement()
     {
-        movement.x =  Input.GetAxisRaw("Horizontal");
-        movement.y = 0f;
-        movement.z = Input.GetAxisRaw("Vertical");
+    //    movement.x =  Input.GetAxisRaw("Horizontal");
+    //    movement.y = 0f;
+    //    movement.z = Input.GetAxisRaw("Vertical");
+        if(Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
     }
 
     private void Update()
     {
-        Movement();
+        //Movement();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
     }
     
 }
