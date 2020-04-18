fec7ef4a9e2e714fbfe86eff922cee6bbf5d2bad
SereneYeti <40300212+SereneYeti@users.noreply.github.com>
2020-04-18 15:51:28 +0200

    Setup Camera Follow script & Resource Inheritance
    -Setup a basic camera follow script
    -Setup Resource inhertance structure with oxygen and health

---
+++ Assets/Scripts/OxygenController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenController : ResourceClass
+{
+    //Main resources
+    public float Heath
+    {
+        get { return base.health; }
+        set { base.health = value; }
+    }
+
+    public float MaxHeath
+    {
+        get { return base.maxHealth; }
+        set { base.maxHealth = value; }
+    }
+
+    public float O2
+    {
+        get { return base.o2; }
+        set { base.o2 = value; }
+    }
+
+    public float MaxO2
+    {
+        get { return base.maxO2; }
+        set { base.maxO2 = value; }
+    }
+
+    public bool IsDead
+    {
+        get { return base.isDead; }
+        set { base.isDead = value; }
+    }
+
+    //Ammo needed to be picked up
+    public float GrenadeAmount
+    {
+        get { return base.grenadeAmount; }
+        set { base.grenadeAmount = value; }
+    }
+
+    public float FlamethrowerFuel
+    {
+        get { return base.flamethrowerFuel; }
+        set { base.flamethrowerFuel = value; }
+    }
+
+    public override void Breathe()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Death()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/ResourceClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ResourceClass
+{
+    //Main resources
+    protected float health;
+    protected float maxHealth;
+    protected float o2;
+    protected float maxO2;
+    protected bool isDead;
+
+    //Ammo needed to be picked up
+    protected float grenadeAmount;
+    protected float flamethrowerFuel;
+
+    //Methods
+    public abstract void TakeDamage();
+    public abstract void Breathe();
+    public abstract void Death();
+
+
+
+}
