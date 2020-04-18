9489d55f0d9178615c64cadfa237b4b714b69d05
SereneYeti <40300212+SereneYeti@users.noreply.github.com>
2020-04-18 13:25:15 +0200

    Added basic WASD movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody rb;
+
+
+    Vector3 movement = new Vector3();
+    private void Start()
+    {
+        
+    }
+
+    public void Movement()
+    {
+        movement.x =  Input.GetAxisRaw("Horizontal");
+        movement.y = 0f;
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+    
+}
